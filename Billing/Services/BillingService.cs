04ed294fd4c696c973e5301011a0802be3d9c7fd
akiri <mistguise@gmail.com>
2022-11-29 17:25:59 +0300

    Add BillingService

---
+++ Billing/Services/BillingService.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+
+namespace Billing
+{
+    public class BillingService : Billing.BillingBase
+    {
+        public BillingService() { }
+
+        public override Task ListUsers(None request, IServerStreamWriter<UserProfile> responseStream, ServerCallContext context)
+        {
+            return base.ListUsers(request, responseStream, context);
+        }
+
+        public override Task<Response> CoinsEmission(EmissionAmount request, ServerCallContext context)
+        {
+            return base.CoinsEmission(request, context);
+        }
+
+        public override Task<Response> MoveCoins(MoveCoinsTransaction request, ServerCallContext context)
+        {
+            return base.MoveCoins(request, context);
+        }
+
+        public override Task<Coin> LongestHistoryCoin(None request, ServerCallContext context)
+        {
+            return base.LongestHistoryCoin(request, context);
+        }
+    }
+}
